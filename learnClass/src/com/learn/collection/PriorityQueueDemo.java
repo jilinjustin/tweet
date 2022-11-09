9e2c5d514ad5b179009f7336ca6d5c7462ccb4e4
unknown <cogjava4344@iiht.tech>
2022-11-09 18:38:28 +0530

    tpriorityqueuedemodemo

--- learnClass/src/com/learn/collection/PriorityQueueDemo.java
+++ learnClass/src/com/learn/collection/PriorityQueueDemo.java
@@ -1,22 +1,25 @@
 package com.learn.collection;
 
 import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.PriorityQueue;
 
-public class TreeSetDemo {
+public class PriorityQueueDemo {
 	
 	public static void main(String[] args) {
-		TreeSet<String> members = new TreeSet<String>();
+		PriorityQueue<String> members = new PriorityQueue<String>();
 		
 		members.add("jilin");
 		members.add("pepper");
 		members.add("jelly");
 		
-		Iterator<String> itr = members.descendingIterator();
+		System.out.println("First one is " + members.peek());
+		
+		Iterator<String> itr = members.iterator();
 		
 		while(itr.hasNext()) {
 			System.out.println(itr.next());
 		}
+		
 	}
 
 }
